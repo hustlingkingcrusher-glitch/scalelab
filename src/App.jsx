9aa47be5afc1e3d2da41c7103a4ae867e4ef5309
hustlingkingcrusher-glitch <hustlingkingcrusher@gmail.com>
2026-08-25 10:25:35 -0700

    Initial ScaleLab foundation

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="app">
+      <main className="app-main">
+        <h1>ScaleLab</h1>
+        <p>Building the foundation.</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
